50bfddac570a35e4fe0d1193bd25795c1a9a5cdc
IdaMathilde <idamathilde.96@gmail.com>
2026-09-14 21:15:57 +0200

    Lagd kort med forside og bakside. Og lagt til tilgjengelighet

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -2,6 +2,7 @@ import type { PlayingCard } from "../../types/game.ts";
 import "./Card.css";
 type CardProps = {
   card: PlayingCard;
+  faceDown?: boolean;
 };
 const suitSymbols = {
     hearts: "♥",
@@ -9,11 +10,46 @@ const suitSymbols = {
     clubs: "♣",
     spades: "♠",
 };
-export default function Card({ card }: CardProps) {
+const suitNames = {
+    hearts: "hjerter",
+    diamonds: "ruter",
+    spades: "spar",
+    clubs: "kløver"
+} 
+ const rankNames = {
+        A: "ess",
+        K: "konge",
+        Q: "dame", 
+        J: "knekt"
+    }
+export default function Card({ card, faceDown = false }: CardProps) {
+  const rankName = 
+    card.rank === "A" ||
+    card.rank === "K" ||
+    card.rank === "Q" ||
+    card.rank === "J" 
+    ?rankNames [card.rank]
+    : card.rank;
+
+  
+    if(faceDown) {
+        return(
+            <div
+            role="img"
+            aria-label="kort med baksiden opp" 
+            className="card card-back">
+                <span className="card-back-top" aria-hidden="true">╔</span>
+                <span className="card-back-bottom" aria-hidden="true">╝</span>
+            </div>
+        );
+    }
     return(
-        <div className="card">
-        <span className="card-rank">{card.rank}</span>
-        <span className="card-suit">{suitSymbols[card.suit]}</span>
+        <div
+        role="img"
+        aria-label={`${suitNames[card.suit]} ${rankName}`}
+                 className="card">
+        <span className="card-rank" aria-hidden="true">{card.rank}</span>
+        <span className="card-suit" aria-hidden="true">{suitSymbols[card.suit]}</span>
         </div>
     );
 }
